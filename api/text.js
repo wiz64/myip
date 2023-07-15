072387b185b24554f7315ae5e9d7284696a6338e
wiz64 <67432394+wiz64@users.noreply.github.com>
2023-07-15 19:09:15 +0530

    Update text.js

--- api/text.js
+++ api/text.js
@@ -2,5 +2,5 @@ const requestIp = require('request-ip');
 
 module.exports = (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
-  res.status(200).send("403");
+  res.status(200).send("401");
 }
